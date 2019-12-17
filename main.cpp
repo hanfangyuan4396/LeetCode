9e0d705e1e6edd686c09f8d147dc89e0d4a19da5
hanfangyuan <hanfangyuan_ustb@163.com>
2019-12-17 19:33:26 +0800

    78_1

---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int Dec2Bin(int dec, int len, vector<int>& bin);
+vector<int> ReverseVector(vector<int>& vec);
+int power(int times);
+vector<vector<int>> subsets(vector<int>& nums);
+int main()
+{
+	vector<int> nums;
+	nums.push_back(1);
+	nums.push_back(2);
+	nums.push_back(3);
+	nums.push_back(4);
+	vector<vector<int>> sets(subsets(nums));
+	for (int i = 0; i < power(nums.size()); i++)
+	{
+		for (int j = 0; j < sets[i].size(); j++)
+		{
+			cout << sets[i][j] << "    ";
+		}
+		cout << endl;
+	}
+	cout << "This is a test!";
+	cout << "This is a second test!";
+	system("pause");
+	return 0;
+}
+int Dec2Bin(int dec, int len, vector<int>& bin)
+{
+	int weight;
+	for (int i = 0; i < len; i++)
+	{
+		bin[i] = 0;
+	}
+	for (int i = len - 1; i >= 0; i--)
+	{
+		 weight = power(i);
+		cout << "i:" << i << "  " << "weight:" << weight << endl;
+		if (dec >= weight)
+		{
+			if (dec == weight)
+			{
+				bin[i] = 1;
+				return 0;
+			}
+			else
+			{
+				bin[i] = 1;
+				dec = dec - weight;
+			}
+		}
+	}
+}
+
+vector<int> ReverseVector(vector<int>& vec)
+{
+	vector<int> revVec;
+	vector<int>::reverse_iterator it;
+	for (it = vec.rbegin(); it != vec.rend(); it++)
+	{
+		revVec.push_back(*it);
+	}
+	return revVec;
+}
+
+int power(int times)
+{
+	int result = 1;
+	for (int i = 0; i < times; i++)
+	{
+		result = 2 * result;
+	}
+	return result;
+}
+vector<vector<int>> subsets(vector<int>& nums)
+{
+	vector<vector<int>> subVec;
+	vector<int> temp;
+	int len = nums.size();
+	vector<int> bin(len);
+	for (int i = 0; i < power(len); i++)
+	{
+		Dec2Bin(i, len, bin);
+		for (int j = 0; j < len; j++)
+		{
+			if (bin[j] == 1)
+			{
+				temp.push_back(nums[j]);
+			}
+		}
+		subVec.push_back(temp);
+		temp.clear();
+	}
+	return subVec;
+	
+}
